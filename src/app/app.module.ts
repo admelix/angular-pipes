4ce67e9edf4fda3353c4aa5f79e09a47e6acdc79
Jose sakuda <sakudacastro@gmail.com>
2020-08-06 03:10:13 -0500

    Usus de pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import  localPe  from '@angular/common/locales/es-PE';
 
 import { AppComponent } from './app.component';
+
+registerLocaleData(localPe);
 
 @NgModule({
   declarations: [
@@ -10,7 +14,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-PE'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
